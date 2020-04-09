920699cd4e2f230622c2529c68366b90b738f4ef
Mosharraf <turash.mosharraf@sap.com>
2020-04-08 22:18:11 -0700

    parse_medication updated

--- parse_medication.py
+++ parse_medication.py
@@ -43,6 +43,20 @@ for i in range(len(medication_list)):
             tex=chunk.text.lower()
             if any(sub in tex for sub in dosage_indicators): dosages.append(tex)
             else: timings.append(tex)
+        else:
+            word_a=chunk.root.text
+            for token in doc:
+                word_b=token.text.lower()
+                if word_a!=word_b:
+                    continue
+                lem=token.lemma_.lower()
+                #print(lem, token.pos_)
+                if token.pos_=="NOUN" and token.tag_=="NN":
+                    s_lower= chunk.text.strip().lower()
+                    if 'purpose' not in s_lower and 'medication' not in s_lower and 'dosage' not in s_lower and 'timing' not in s_lower:medications.append(chunk.text)
+                    s_lower= chunk.text.strip().lower()
+                    if 'purpose' not in s_lower and 'medication' not in s_lower and 'dosage' not in s_lower and 'timing' not in s_lower:purposes.append(chunk.text) 
+
     
     
     parts=u.split()
@@ -62,22 +76,72 @@ for i in range(len(medication_list)):
             t=m_elem[0]+m_elem[1]+m_elem[2]
             dosages.append(t)
     dosages=list(set(dosages))
-
+    '''
     m = re.findall('(take|taking|took|taken|takes|taken)(.*?)(\\.|for|since)', u)
     
     if m!=None:
         for m_elem in m:
             t=m_elem[1]
             medications.append(t)
+    '''
     medications=list(set(medications))
     
-    m = re.findall('(take|taking|took|taken|takes|taken|use|using|used|uses|)(.*?)(for)(.+?)(\\.)', u)
     
+    m = re.findall('(take|taking|took|taken|takes|taken|use|using|used|uses)(.+?)(for)(.+?)(\\.)', u)
+    
+
     if m!=None:
         for m_elem in m:
+            #print(m_elem)
             t=m_elem[3]
-            purposes.append(t)
+            s=m_elem[1]
+            t_parts=t.split()
+            s_parts=s.split()
+            if len(t_parts)<=2: 
+                purposes.append(t.strip().lower())
+            if len(s_parts)<=2: 
+                medications.append(s.strip().lower())
+    
     purposes=list(set(purposes))
+    medications=list(set(medications))
+
+    m = re.findall('(medication |medication:|medications |medications:)(.+?)(,|\\.|and)', u)
+    if m!=None:
+        for m_elem in m:
+            s=m_elem[1]
+            if len(s_parts)<=2:
+                s_lower= s.strip().lower()
+                medications.append(s_lower)
+    medications=list(set(medications))
+    
+    m = re.findall('(purpose |problem |purpose:|purposes |purposes:)(.+?)(,|\\.|and)', u)
+    if m!=None:
+        for m_elem in m:
+            s=m_elem[1]
+            if len(s_parts)<=2: 
+                s_lower= s.strip().lower()
+                purposes.append(s_lower)
+    purposes=list(set(purposes))
+
+    m = re.findall('(timing |timing:|timings |timings:)(.+?)', u)
+    if m!=None:
+        for m_elem in m:
+            s=m_elem[1]
+            if len(s_parts)<=2: 
+                s_lower=s.strip().lower()
+                if s_lower!="":
+                    timings.append(s_lower)
+    timings=list(set(timings))
+
+    m = re.findall('(dosage |dosage:|dosages |dosages:)(.+?)', u)
+    if m!=None:
+        for m_elem in m:
+            s=m_elem[1]
+            if len(s_parts)<=2: 
+                s_lower=s.strip().lower()
+                if s_lower!="":
+                    dosages.append(s_lower)
+    dosages=list(set(dosages))
 
     print("response: ")
     print(doc)
